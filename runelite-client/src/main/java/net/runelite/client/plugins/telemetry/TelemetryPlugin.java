f1930b1ca8c50e3d5c0ad4adb6e862321c94ef08
TheStonedTurtle <29030969+TheStonedTurtle@users.noreply.github.com>
2018-11-26 12:48:05 -0800

    Event Loot Telemetry
    Copy pasted from Loot Tracker

--- runelite-client/src/main/java/net/runelite/client/plugins/telemetry/TelemetryPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/telemetry/TelemetryPlugin.java
@@ -28,20 +28,35 @@ package net.runelite.client.plugins.telemetry;
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
+import net.runelite.api.InventoryID;
+import net.runelite.api.ItemContainer;
 import net.runelite.api.NPC;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.NpcSpawned;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.widgets.WidgetID;
 import net.runelite.client.events.NpcLootReceived;
+import net.runelite.client.game.ItemStack;
 import net.runelite.client.game.TelemetryManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.telemetry.data.EventLootTelemetry;
 import net.runelite.client.plugins.telemetry.data.NpcLootTelemetry;
 import net.runelite.client.plugins.telemetry.data.NpcSpawnedTelemetry;
 import net.runelite.client.task.Schedule;
+import net.runelite.client.util.Text;
 
 @PluginDescriptor(
 	name = "Telemetry Plugin",
@@ -54,8 +69,13 @@ public class TelemetryPlugin extends Plugin
 	// 5 Minute in Milliseconds
 	private static final int TIME_EXPIRE_PERIOD = 5 * 60 * 1000;
 
+	// Event Loot System
+	private static final Pattern CLUE_SCROLL_PATTERN = Pattern.compile("You have completed [0-9]+ ([a-z]+) Treasure Trails.");
+	private static final int THEATRE_OF_BLOOD_REGION = 12867;
+
 	private boolean ignoreTick;
 	private int tickCount;
+	private String eventType;
 
 	@Inject
 	private Client client;
@@ -117,6 +137,93 @@ public class TelemetryPlugin extends Plugin
 			telemetryManager.submit(new NpcSpawnedTelemetry(n.getId(), n.getWorldLocation()));
 		}
 
+	}
+
+	@Subscribe
+	public void onWidgetLoaded(WidgetLoaded event)
+	{
+		final ItemContainer container;
+		switch (event.getGroupId())
+		{
+			case (WidgetID.BARROWS_REWARD_GROUP_ID):
+				eventType = "Barrows";
+				container = client.getItemContainer(InventoryID.BARROWS_REWARD);
+				break;
+			case (WidgetID.CHAMBERS_OF_XERIC_REWARD_GROUP_ID):
+				eventType = "Chambers of Xeric";
+				container = client.getItemContainer(InventoryID.CHAMBERS_OF_XERIC_CHEST);
+				break;
+			case (WidgetID.THEATRE_OF_BLOOD_GROUP_ID):
+				int region = WorldPoint.fromLocalInstance(client, client.getLocalPlayer().getLocalLocation()).getRegionID();
+				if (region != THEATRE_OF_BLOOD_REGION)
+				{
+					return;
+				}
+				eventType = "Theatre of Blood";
+				container = client.getItemContainer(InventoryID.THEATRE_OF_BLOOD_CHEST);
+				break;
+			case (WidgetID.CLUE_SCROLL_REWARD_GROUP_ID):
+				// event type should be set via ChatMessage for clue scrolls.
+				// Clue Scrolls use same InventoryID as Barrows
+				container = client.getItemContainer(InventoryID.BARROWS_REWARD);
+				break;
+			default:
+				return;
+		}
+
+		if (container == null)
+		{
+			return;
+		}
+
+		// Convert container items to array of ItemStack
+		final Collection<ItemStack> items = Arrays.stream(container.getItems())
+			.filter(item -> item.getId() > 0)
+			.map(item -> new ItemStack(item.getId(), item.getQuantity()))
+			.collect(Collectors.toList());
+
+		if (!items.isEmpty())
+		{
+			telemetryManager.submit(new EventLootTelemetry(eventType, items));
+		}
+		else
+		{
+			log.debug("No items to find for Event: {} | Container: {}", eventType, container);
+		}
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
+		if (event.getType() != ChatMessageType.SERVER && event.getType() != ChatMessageType.FILTERED)
+		{
+			return;
+		}
+
+		// Check if message is for a clue scroll reward
+		final Matcher m = CLUE_SCROLL_PATTERN.matcher(Text.removeTags(event.getMessage()));
+		if (m.find())
+		{
+			final String type = m.group(1).toLowerCase();
+			switch (type)
+			{
+				case "easy":
+					eventType = "Clue Scroll (Easy)";
+					break;
+				case "medium":
+					eventType = "Clue Scroll (Medium)";
+					break;
+				case "hard":
+					eventType = "Clue Scroll (Hard)";
+					break;
+				case "elite":
+					eventType = "Clue Scroll (Elite)";
+					break;
+				case "master":
+					eventType = "Clue Scroll (Master)";
+					break;
+			}
+		}
 	}
 
 	@Schedule(
